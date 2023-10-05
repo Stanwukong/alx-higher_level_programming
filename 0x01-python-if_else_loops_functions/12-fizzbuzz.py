d136af4e934ba49b8391d24742c86b06102237f9
Stanwukong <stanleyajanaku@gmail.com>
2023-10-04 18:09:53 -0700

    Task 12: Looks like fizzbuzz is native

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(0, 101):
-        if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz")
-        elif num % 3 == 0 and num % 5 != 0:
-            print("Fizz")
-        elif num % 3 != 0 and num % 5 == 0:
-            print("Buzz")
-        else:
-            print(num)
