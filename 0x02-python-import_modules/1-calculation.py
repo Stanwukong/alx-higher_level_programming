ef995cf8c095eea37d46c0d389c4946899d363b9
Stanwukong <stanleyajanaku@gmail.com>
2023-10-07 14:44:10 -0700

    Task 1: Second try

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,10 +2,12 @@
 # 1-calculation.py
 
 if __name__ == "__main__":
-    """Does some Maths, and prints the result"""
+    """Does some Maths, and prints the result."""
     from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
+
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
