860dd18b4f3262f2c65662f2682e0bae6b2c56b0
Stanwukong <stanleyajanaku@gmail.com>
2023-10-07 13:52:00 -0700

    Not sure why it's still failing checks

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+# 0_add.py
 
-""" Imports the function from add_0.py
-
-   prints the result of the addition 1 + 2 = 3
-
-   Assign:
-         a = 1, b = 2
-"""
 if __name__ == "__main__":
-
+    """Print the sum of a and b."""
     from add_0 import add
 
     a = 1
