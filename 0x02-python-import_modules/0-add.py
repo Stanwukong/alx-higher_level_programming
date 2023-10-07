c7a4dbbbe7b55f915ee91cd5b555b274212df326
Stanwukong <stanleyajanaku@gmail.com>
2023-10-07 13:47:05 -0700

    Modified import statement and code execution

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,9 +7,10 @@
    Assign:
          a = 1, b = 2
 """
-import add_0
+if __name__ == "__main__":
 
-a = 1
-b = 2
-result = add_0.add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
