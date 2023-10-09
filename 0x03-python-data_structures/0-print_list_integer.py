09495369b1aab8994bc68630606f4e2aa5aedec9
Stanwukong <stanleyajanaku@gmail.com>
2023-10-09 15:05:06 -0700

    Task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    """Prints all integers of a list"""
+    for num in range(len(my_list)):
+        print("{}".format(my_list[i]))
