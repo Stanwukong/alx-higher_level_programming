83f3d421b1b6da7ef4201b03f2d21bea9f240769
Stanwukong <stanleyajanaku@gmail.com>
2023-10-26 07:15:17 -0700

    Task 1: Check 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Defines a square"""
+
+class Square:
+    """Instanced with size"""
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args: Size
+        """
+
+        self.__size = size
