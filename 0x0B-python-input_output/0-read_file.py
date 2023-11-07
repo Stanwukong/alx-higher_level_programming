51ef43317109a502faed7f62bd707f266b2f16e8
Stanwukong <stanleyajanaku@gmail.com>
2023-11-07 09:18:41 -0800

    Task 0: Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
+def read_file(filename=""):
+    """
+    Read a text file (UTF-8) ad print it to stdout.
+
+    Args:
+        filename (str): The path to the file. Default is an empty string.
+    """
+    with open(filename, mode="r", encoding="UTF-8") as myFile:
+        data = myFile.read()
+        print(data)
