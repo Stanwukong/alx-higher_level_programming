7ab1d3ce8bc1508b4927460ceded690a58843997
Stanwukong <stanleyajanaku@gmail.com>
2023-11-06 22:58:42 -0800

    Task 6: Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """This represents the class."""
+
+    def area(self):
+        """
+        Raises an Exception.
+        """
+        raise Exception('area() is not implemented')
