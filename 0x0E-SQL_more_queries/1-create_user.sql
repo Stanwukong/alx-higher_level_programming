f8ccb25632b0742fbdba429e34f726d12eb09f4b
Stanwukong <stanleyajanaku@gmail.com>
2023-12-14 07:48:28 -0800

    Corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,3 +3,4 @@
 -- and grants all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON "." TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
