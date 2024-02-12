fe79a9d0d440c5208738c8d910c3d3ce0f20b995
Stanwukong <stanleyajanaku@gmail.com>
2024-02-12 06:26:35 -0800

    Tasks completed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-let noArgs = argv.length;
-console.log(noArgs === 2 ? 'No argument' : noArgs == 3 ? 'Argument found' : 'Arguments found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
